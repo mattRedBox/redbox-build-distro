b83f36dd2e7d20706b5e14cf08937682b83969d2
mattRedBox <matt@redboxresearchdata.com.au>
2016-02-19 09:50:12 +1000

    test initial selenium harness.

---
+++ src/test/java/au/com/redboxresearchdata/test/selenium/prototype/Selenium2ExamplesTest.java
@@ -0,0 +1,22 @@
+package au.com.redboxresearchdata.test.selenium.prototype;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ * @author <a href="matt@redboxresearchdata.com.au">Matt Mulholland</a>
+ */
+public class Selenium2ExamplesTest {
+    @Test
+    public void test() {
+        // Instantiate a webDriver implementation
+        WebDriver webdriver = new HtmlUnitDriver();
+        webdriver.get("https://centos7-test-2.redboxresearchdata.com.au/redbox");
+        Assert.assertEquals("Home - Everything - ReDBox", webdriver.getTitle());
+        webdriver.quit();
+    }
+}
